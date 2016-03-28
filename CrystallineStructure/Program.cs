d75822def75897f60fd867b1b186438cef0f5c64
Isaac Styles <isaacstyles92@gmail.com>
2016-03-28 04:16:42 -0400

    first commit

---
+++ CrystallineStructure/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CrystallineStructure
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UnitCell uc = new UnitCell();
+			uc.Add(new LatticePoint("Cl", -1, 0, 0, 0, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 1, 0, 0, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 0, 1, 0, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 0, 0, 1, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 1, 1, 0, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 1, 0, 1, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 0, 1, 1, 1.16F));
+			uc.Add(new LatticePoint("Cl", -1, 1, 1, 1, 1.16F));
+			uc.Add(new LatticePoint("Cs", 1, .5F, .5F, .5F, 1.88F));
+			uc.ValidateBCCCell();
+			Space s = new Space();
+			s.CubicFill(uc, 4, 0, 0, 0);
+
+			List<int> hull = Space.CubicConvexHull(ref s);
+		}
+	}
+
+	class LatticePoint
+	{
+		public float x, y, z, r;
+		public string element;
+		public sbyte charge;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LatticePoint" /> class.
+		/// </summary>
+		/// <param name="e">The element name.</param>
+		/// <param name="x">The x coordinate within a unit cell. values are 0 - 1.</param>
+		/// <param name="y">The y coordinate within a unit cell. values are 0 - 1.</param>
+		/// <param name="z">The z coordinate within a unit cell. values are 0 - 1.</param>
+		/// <param name="r">The radius of the complete atom in angstroms. 1A = 100 picometers.</param>
+		/// <param name="c">The charge of the ion.</param>
+		public LatticePoint(string e, sbyte c, float x, float y, float z, float r)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.r = r;
+			element = e;
+			charge = c;
+		}
+	}
+	class UnitCell
+	{
+		public List<LatticePoint> Atoms;
+		public float dimX, dimY, dimZ;
+		public int pCation = -1, pAnion = -1;           //pointers to cation and anion
+		public UnitCell()
+		{
+			Atoms = new List<LatticePoint>();
+			dimX = 0;
+			dimY = 0;
+			dimZ = 0;
+		}
+		public bool Add(LatticePoint a)
+		{
+
+			for (int i = 0; i < Atoms.Count; i++)   //check that the coordinate is not already occupied. Allows overlapping orbitals
+			{
+				if (Atoms[i].x == a.x && Atoms[i].y == a.y && Atoms [i].z == a.z)
+				{
+					return false;                       //return the index of the atom occupying the coordinate
+				}
+			}
+			Atoms.Add(a);
+			return true;                              //return -1 if successful			
+		}
+		public bool AddFCC(string e,  float r, sbyte charge)
+		{
+			float[,] Positions = new float[,] { { 0, 0, 0 }
+											  , { 0, 0, 1 }
+											  , { 0, 1, 0 }
+											  , { 0, 1, 1 }
+											  , { 1, 0, 0 }
+											  , { 1, 0, 1 }
+											  , { 1, 1, 0 }
+											  , { 1, 1, 1 }
+											  , { 0, .5F, .5F }
+											  , { .5F, 0, .5F }
+											  , { .5F, .5F, 0 }
+											  , { 1, .5F, .5F }
+											  , { .5F, 1, .5F }
+											  , { .5F, .5F, 1 } };
+			for (int i = 0; i < Positions.GetUpperBound(0); i++)
+			{
+                if (!Add(new LatticePoint(e, charge, Positions[i, 0], Positions[i, 1], Positions[i, 2], r)))//if location is already occupied
+                {
+                    return false;                    
+                }
+            }
+			return true;
+		}
+		public void Resize ()
+		{
+
+			if (Atoms.Count == 1)                   //if simple cubic cell
+			{
+				dimX = Atoms[0].r * 2;              //set cell size to 2*radius
+				dimY = Atoms[0].r * 2;
+				dimZ = Atoms[0].r * 2;
+				Atoms[0].x = .5F;                    //center atom in cell
+				Atoms[0].y = .5F;
+				Atoms[0].z = .5F;
+			}
+			/*if (Atoms.Count >= 8)
+			{
+				//double maxX=0, maxY=0, maxZ=0;
+				for (int i = 0; i < Atoms.Count-1; i++)
+				{
+				   for (int j = i+1; j < Atoms.Count; j++)
+				   {
+							 if (Atoms[i].x != Atoms[j].x && Atoms[i].y == Atoms[j].y && Atoms [i].z == Atoms [j].z && Atoms[i].r + Atoms [j].r > dimX )
+					   {     
+							dimX= Atoms[i].r + Atoms [j].r;
+					   }
+					   else  if ( Atoms[i].y != Atoms[j].y && Atoms[i].x == Atoms[j].x && Atoms [i].z == Atoms [j].z && Atoms[i].r + Atoms [j].r > dimY )
+					   {
+							dimY = Atoms [i].r + Atoms[j].r;
+					   }
+					   else  if ( Atoms[i].z != Atoms[j].z && Atoms[i].x == Atoms[j].x && Atoms [i].y == Atoms [j].y && Atoms[i].r + Atoms [j].r > dimZ )
+					   {
+							dimZ = Atoms [i].r + Atoms[j].r;
+					   }
+
+					}
+				}
+			}
+			else
+			{}
+		 return true; */
+
+
+			//this loop measures distance between all pairs of atoms and outputs pairs that overlap
+			/*for (int p = 0; p < Atoms.Count-1 && Atoms.Count > 0; p++)                          //start at first atom, ignoring the last
+			{
+
+				for (int i = p+1; i < Atoms.Count; i++)                                        //compare atom to each successive atom
+				{
+					if ( Math.Sqrt((Atoms[i].x - Atoms[p].x) * (Atoms[i].x - Atoms[p].x)        //if distance between atoms is less than the two radii
+								 + (Atoms[i].y - Atoms[p].y) * (Atoms[i].y - Atoms[p].y)
+								 + (Atoms[i].z - Atoms[p].z) * (Atoms[i].z - Atoms[p].z)) < Atoms[i].r + Atoms[p].r)
+					{
+						//if new atom overlaps another atom
+						return new int[] { p,i };                   //return zero-based index of atom which overlaps 
+					}
+				} 
+			}*/
+
+
+
+		}
+		
+		public bool ValidateBCCCell()
+		{
+			float[,] Positions = new float[,] { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 }, { 0, 1, 1 }, { 1, 0, 0 }, { 1, 0, 1 }, { 1, 1, 0 }, { 1, 1, 1 }, { .5F, .5F, .5F } };
+			int[] Found = new int[9];
+			for (int i = 0; i < Atoms.Count; i++)
+			{
+				for (int j = 0; j < Positions.Length; j++)
+				{
+					if (Atoms[i].x == Positions[j, 0] && Atoms[i].y == Positions[j, 1] && Atoms[i].z == Positions[j, 2])
+					{
+						Found[j]++;
+						break;
+					}
+
+				}
+				if (Atoms[i].x == .5 && Atoms[i].y == .5 && Atoms[i].z == .5)
+				{
+					pAnion = i;
+				}
+				else
+				{
+					if (pCation != -1 && Atoms[pCation].r != 0 && Atoms[i].r != Atoms[pCation].r)
+					{
+						throw new Exception("Corner Atoms are not same radius");
+					}
+					pCation = i;
+
+				}
+			}
+			for (int i = 0; i < Found.Length; i++)
+			{
+				if (Found[i] != 1)
+				{
+					throw new Exception("Lattice Point is missing or already exists");
+				}
+			}
+			/*r0 and r1 set*/
+			float l = (2 * Atoms[pCation].r + 2 * Atoms[pAnion].r) / (float)Math.Sqrt(3);    //length of side of cubic unit cell
+			this.dimX = l;
+			this.dimY = l;
+			this.dimZ = l;
+			return true;
+		}
+
+		public bool ValidateFCCCell()
+		{
+			float[,] Positions = new float[,] { { 0, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 }, { 0, 1, 1 }, { 1, 0, 0 }, { 1, 0, 1 }, { 1, 1, 0 }, { 1, 1, 1 }, { 0, .5F, .5F }, { .5F, 0, .5F }, { .5F, .5F, 0 }, { 1, .5F, .5F }, { .5F, 1, .5F }, { .5F, .5F, 1 } };
+			int[] Found = new int[14];
+			for (int i = 0; i < Atoms.Count; i++)
+			{
+				for (int j = 0; j < Positions.Length; j++)
+				{
+					if (Atoms[i].x == Positions[j,0] && Atoms[i].y == Positions[j,1] && Atoms[i].z == Positions[j,2])
+					{
+						Found[j]++;
+						break;
+					}
+				   
+				}
+				if (Atoms[i].x == .5 || Atoms[i].y == .5 || Atoms[i].z == .5)
+				{
+					if (pAnion == -1)
+					{
+						pAnion = i;                        
+					}
+					if (Atoms[i].r != Atoms[pAnion].r || Atoms[i].element != Atoms[pAnion].element)
+					{
+						throw new Exception("Anions are not same element");
+					}
+				}
+				else
+				{
+					if (Atoms[i].x == 0 || Atoms[i].x == 1 && Atoms[i].y == 0 || Atoms[i].y == 1 &&Atoms[i].z == 0 || Atoms[i].z == 1)
+					{
+						if (pCation == -1)
+						{
+							pCation = 1;
+						}
+						if (Atoms[i].r != Atoms[pCation].r || Atoms[i].element != Atoms[pCation].element)
+						{
+							throw new Exception("Cations are not same element");
+						} 
+					}
+				}
+			}
+			for (int i = 0; i < Found.Length; i++)
+			{
+				if (Found[i] != 1)
+				{
+					throw new Exception("Lattice Point is missing or already exists");
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ CrystallineStructure/Space.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystallineStructure
+{
+	class Locale
+	{
+		public string element;
+		public float[] coord = new float[3];
+		public float r;
+		public sbyte charge;
+
+		public Locale(string element, sbyte charge, float x,float y,float z)
+		{
+			this.element = element;
+			this.charge = charge;
+			coord[0] = x;
+			coord[1] = y;
+			coord[2] = z;
+		}
+	}
+	class Space
+	{
+		public List<Locale> Atoms = new List<Locale>(26214400);
+		public int length = 0;
+		public Space()
+		{ }
+
+
+		public void CubicFill(UnitCell a, int n, float offX, float offY, float offZ)
+		{
+			length = n;
+			for (int x = 0; x < n; x++)
+			{
+				for (int y = 0; y < n; y++)
+				{
+					for (int z = 0; z < n; z++)
+					{
+						Mount(ref a, x, y, z, offX, offY, offZ);
+						System.Diagnostics.Debug.WriteLine(x + " " + y + " " + z);
+					} 
+				}				
+			}
+		}
+		private void Mount(ref UnitCell a, int x, int y, int z, float offX, float offY, float offZ)
+		{
+			Atoms.Add(new Locale(a.Atoms[a.pAnion].element, a.Atoms[a.pAnion].charge, a.dimX*x + a.Atoms[a.pAnion].x * a.dimX + offX, a.dimY*y + a.Atoms[a.pAnion].y * a.dimY + offY, a.dimZ*z + a.Atoms[a.pAnion].z * a.dimZ + offZ));
+		
+		}
+
+		/// <summary>
+		/// Input: integer coordinates (X,Y,Z) of a locale in a simple cubic structure
+		/// </summary>
+		/// <param name="c">The coordinates</param>
+		/// <param name="l">The length of side of cube</param>
+		/// <returns>index in array</returns>
+		public static int CubicCoordinateToPointer(int l, int[] c)
+		{
+			int p = 0;
+
+			p += c[0] * l * l;
+
+			p += c[1] * l;
+			p += c[2];
+			return p;
+		}
+
+		public  static List<int> CubicConvexHull(ref Space s)
+		{
+			if (Math.Pow(s.length,3) != (double)s.Atoms.Count)  //if cubic root is not an integer then structure is not a cube
+			{
+				return null;
+			}
+			int x = s.length;     //x is length of side of cube
+
+			List<int> hull = new List<int>();
+			for (int i = 0; i < x; i++)
+			{
+				for (int j = 0; j < x; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length,new int[] { i, 0, j }));   //Left face  
+					
+				}
+			}
+			for (int i = 1; i < x; i++)
+			{
+				for (int j = 0; j < x; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length, new int[] { 0, i, j }));   //Back face                
+				}
+			}
+			for (int i = 1; i < x; i++)
+			{
+				for (int j = 1; j < x; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length, new int[] { i, j, 0 }));   //Bottom face                    
+				}
+			}
+			for (int i = 1; i < x; i++)
+			{
+				for (int j = 1; j < x; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length, new int[] { x - 1, i, j }));   //Front face                    
+				}
+			}
+			for (int i = 1; i < x-1; i++)
+			{
+				for (int j = 1; j < x-1; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length, new int[] { i, j, x - 1 }));   //Top face                    
+				}
+			}
+			for (int i = 1; i < x-1; i++)
+			{
+				for (int j = 1; j < x; j++)
+				{
+					hull.Add(CubicCoordinateToPointer(s.length, new int[] { i, x - 1, j }));   //Right face                    
+				}
+			}
+			return hull;
+		}
+	}
+}
