ee2a8dea75f0131d136592824c42b0693a708f05
Isaac Styles <styles@etsu.edu>
2016-12-23 03:29:07 -0500

    removed bin, obj directories. Created Balance.cs

---
+++ CrystallineStructure/Balance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystallineStructure
+{
+    class Balance
+    {
+        Dictionary<string, int> Valences = new Dictionary<string, int>
+        {
+            {"H",1},
+            {"Li",1},
+            {"Na",1},
+            {"K",1},
+            {"Rb",1},
+            {"Cs",1},
+            {"Fr",1},
+            {"Be",2},
+            {"Ng",2},
+            {"Ca",2},
+            {"Sr",2},
+            {"Ba",2},
+            {"Ra",2},
+            {"B",3},
+            {"Al",3},
+            {"Ga",3},
+            {"In",3},
+            {"Tl",3},
+            {"C",4},
+            {"Si",4},
+            {"Ge",4},
+            {"Sn",4},
+            {"Pb",4},
+            {"N",5},
+            {"P",5},
+            {"As",5},
+            {"Sb",5},
+            {"Bi",5},
+            {"O",6},
+            {"S",6},
+            {"Se",6},
+            {"Te",6},
+            {"Po",6},
+            {"F",7},
+            {"Cl",7},
+            {"Br",7},
+            {"I",7},
+            {"At",7}
+        };
+
+    }
+}
